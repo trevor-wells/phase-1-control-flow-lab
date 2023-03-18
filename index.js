87f75c7dab062c13a5121b4228a3159361d3d4f9
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-17 21:59:37 -0600

    Final commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet > 2500) {
+    return "No can do.";
+  }
+  else if (feet > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (feet >= 400) {
+    return "That will be twenty bucks.";
+  }
+  else {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const response = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
